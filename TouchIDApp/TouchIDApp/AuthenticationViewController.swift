18d6340f95129268921ed12874e42f5ce9ec45c7
Mohammed Elnaggar <moelnaggar14@gmail.com>
2019-03-21 00:48:16 +0200

    add Project
    - add touchId project

---
+++ TouchIDApp/TouchIDApp/AuthenticationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  TouchIDApp
+//
+//  Created by Mohammed Elnaggar on 3/21/19.
+//  Copyright © 2019 Mohammed Elnaggar. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class AuthenticationViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+    @IBAction func loginButtonWasPressed(_ sender: UIButton) {
+        
+    }
+
+
+}
+
